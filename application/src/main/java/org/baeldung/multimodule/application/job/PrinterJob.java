b8812c0c659877e8fbcdea31b84ff6e8d0130134
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2018-12-26 20:15:34 -0600

    correccion para dependiencias de job (componente)

--- application/src/main/java/org/baeldung/multimodule/application/job/PrinterJob.java
+++ application/src/main/java/org/baeldung/multimodule/application/job/PrinterJob.java
@@ -1,23 +1,35 @@
 package org.baeldung.multimodule.application.job;
 
-import org.baeldung.multimodule.application.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.baeldung.multimodule.library.service.EvenOddService;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Carlos Montoya
  * @since 26/12/2018
  */
+@Component
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Log4j2
 public class PrinterJob extends QuartzJobBean
 {
+	private final EvenOddService evenOddService;
+
 	@Override
 	protected void executeInternal(JobExecutionContext context)
 			throws JobExecutionException
 	{
-		CompanyService companyService = (CompanyService)
-				context.getMergedJobDataMap().get("companyService");
+//		CompanyService companyService = (CompanyService)
+//				context.getMergedJobDataMap().get("companyService");
+//
+//		companyService.printCompany(2001);
 
-		companyService.printCompany(2001);
+		log.info("Printer job is working!... 42 is {}",
+				() -> evenOddService.isEvenOrOdd(42));
 	}
 }
